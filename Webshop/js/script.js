694ebcc42eee7f6a152b4161aa09c36ea0c8b38f
DenchOriginal <d.gusarov@outlook.de>
2017-06-06 16:58:52 +0200

    Webshop Update + JSON

--- Webshop/js/script.js
+++ Webshop/js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
   });
+
+$.getJSON( "js/produkte.json", function( data ) {
+  var items = [];
+  $.each( data["produkte"]["gerichte"], function( key, val ) {
+    $("#card-template").clone().attr("id","gericht_"+key).appendTo("#kacheln").show();
+	$("#gericht_"+key).find(".card-content p").html(val["description"]);
+	$("#gericht_"+key).find(".image").attr("src","img/products/"+val["image"]);
+	$("#gericht_"+key).find(".card-title").html(val["name"]);
+  });
+ 
+});
